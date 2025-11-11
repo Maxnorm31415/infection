b09175f45a9d489ed120902092b6d48b10e82f82
Maxnorm31415 <kibalynik11@gmail.com>
2025-11-11 20:38:23 +0100

    add file person.py with 2 classes(Human, Infection)

---
+++ person.py
@@ -0,0 +1,42 @@
+import random
+
+
+class Human:
+    firstname = ""
+    lastname = ""
+    age = 0
+    social_rang = 0
+    travel_rang = 0
+    pos_x = 0
+    pos_y = 0
+    infection = False
+    def __init__(self, x, y,f_name = "None",l_name = "None",ag = 0,s_r = 0,t_r = 0):
+        if f_name == "None":
+            self.firstname = random.choice(["Alex", "Maya", "Leo", "Sophie", "Ethan", "Luna", "Oliver", "Isla", "Noah", "Aria"])
+            self.lastname = random.choice(["Johnson", "Carter", "Bennett", "Miller", "Collins", "Smith", "Brown", "Davis", "Wilson", "Clark"])
+            self.age = random.randint(15,80)
+            self.social_rang = random.randint(1,10)
+            self.travel_rang = random.randint(1,10)
+        else:
+            self.firstname = f_name
+            self.lastname = l_name
+            self.age = ag
+            self.social_rang = s_r
+            self.travel_rang = t_r
+        self.pos_x = x
+        self.pos_y = y
+
+    def try_speek(self, human):
+        chance = (0.1 * self.social_rang) * (0.1 * human.social_rang)
+        return random.uniform(0, 1) <= chance
+
+class Infected(Human):
+
+    def __init__(self, human):
+        super().__init__(human.pos_x, human.pos_y, human.firstname, human.lastname, human.age, human.social_rang, human.travel_rang)
+        self.infection = True
+
+    def try_travel(self):
+        chance = 0.015*self.travel_rang
+        return random.uniform(0,1)<= chance
+
