926ca3d8b06fd7ec21dc9084217f68aacc526cdc
Maxnorm31415 <kibalynik11@gmail.com>
2025-11-11 23:48:12 +0100

    add continents and humans

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from person import Human, Infected
 
 BLACK = (0, 0, 0)
 
+total_people = 1200 #min 120
+
+continents = {}
+list_humans = []
+list_infected = []
 FRAMES_PER_SECOND = 60
+
+pygame.init()
+
+
 
 def on_the_land(x,y,mask1):
     if mask1.get_at((x,y)):
@@ -12,14 +21,56 @@ def on_the_land(x,y,mask1):
     else:
         return False
 
+def make_continents():
+    global continents, total_people
+    total_people -= 120 #min 20 peoples on 1 continent
+    # For the value max_people, we asked to calculate the percentage of people living on each continent relative to the total world population.
+    # ChatGPT generated the following list with percentages:
+    # Eurasia – 65.7%, Africa – 19.1%, North America – 8.9%, South America – 5.5%, Australia – 0.7%, Greenland – 0.1%.
+    # bounds(x_min,y_min,x_max,y_max)
+    continents = {
+        "north_america":  {"bounds": (277,434,535,748), "max_people": 20 + int(total_people * 0.089), "infection_rang":9},
+        "south_america":  {"bounds": (84,74,380,322), "max_people": 20 + int(total_people * 0.055), "infection_rang":6},
+        "eurasia":        {"bounds": (525,5,676,88), "max_people": 20 + int(total_people * 0.657), "infection_rang":7},
+        "africa":         {"bounds": (636,272,988,710), "max_people": 20 + int(total_people * 0.191), "infection_rang":3},
+        "australia":      {"bounds": (1370,587,1589,737), "max_people": 20 + int(total_people * 0.007), "infection_rang":9},
+        "greenland":      {"bounds": (586,39,1447,378), "max_people": 20 + int(total_people * 0.001), "infection_rang":6},
+    }
+    total_people = 0
+    for continent in continents:
+        total_people += continents[continent]["max_people"]
+
+def check_spawn(x,y,num,board):
+    if board.get_at((x,y)):
+        if len(list_humans) > 0:
+            for j in range((len(list_humans) - num), len(list_humans) - 1):
+                if list_humans[j].pos_x == x and list_humans[j].pos_y == y:
+                    return False
+        return True
+    else: return False
+
+
+def make_humans(boards):
+    global list_humans
+    for cont in continents:
+        for i in range(continents[cont]["max_people"]):
+            finish = False
+            while(not finish):
+                x = random.randint(continents[cont]["bounds"][0],continents[cont]["bounds"][2])
+                y = random.randint(continents[cont]["bounds"][1],continents[cont]["bounds"][3])
+                if check_spawn(x,y,i,boards):
+                    list_humans.append(Human(x,y,continents[cont]))
+                    finish = True
+
 def main():
-    pygame.init()
     screen = pygame.display.set_mode((1600, 821))
     pygame.display.set_caption("Human Infection")
     mask = pygame.mask.from_surface(pygame.image.load('mask.png').convert_alpha())
     world = pygame.image.load('world.png').convert()
     clock = pygame.time.Clock()
     mouse_pos = pygame.mouse.get_pos()
+    make_continents()
+    make_humans(mask)
     while True:
         # event handling
         for event in pygame.event.get():
--- person.py
+++ person.py
@@ -1,5 +1,7 @@
 import random
 
+DARK_GREEN = (14, 112, 7)
+RED = (176, 0, 0)
 
 class Human:
     firstname = ""
@@ -9,20 +11,25 @@ class Human:
     travel_rang = 0
     pos_x = 0
     pos_y = 0
+    continent = {}
     infection = False
-    def __init__(self, x, y,f_name = "None",l_name = "None",ag = 0,s_r = 0,t_r = 0):
+    color = (0,0,0)
+    def __init__(self, x, y, cont,f_name = "None",l_name = "None",ag = 0,s_r = 0,t_r = 0):
         if f_name == "None":
             self.firstname = random.choice(["Alex", "Maya", "Leo", "Sophie", "Ethan", "Luna", "Oliver", "Isla", "Noah", "Aria"])
             self.lastname = random.choice(["Johnson", "Carter", "Bennett", "Miller", "Collins", "Smith", "Brown", "Davis", "Wilson", "Clark"])
             self.age = random.randint(15,80)
             self.social_rang = random.randint(1,10)
             self.travel_rang = random.randint(1,10)
+            self.color = DARK_GREEN
         else:
             self.firstname = f_name
             self.lastname = l_name
             self.age = ag
             self.social_rang = s_r
             self.travel_rang = t_r
+            self.color = RED
+        self.continent = cont
         self.pos_x = x
         self.pos_y = y
 
@@ -34,7 +41,7 @@ class Infected(Human):
     virus_live = 0
     virus_level = 1
     def __init__(self, human):
-        super().__init__(human.pos_x, human.pos_y, human.firstname, human.lastname, human.age, human.social_rang, human.travel_rang)
+        super().__init__(human.pos_x, human.pos_y, human.continent,human.firstname, human.lastname, human.age, human.social_rang, human.travel_rang, human.continent)
         self.infection = True
         self.virus_live = int(0.346*human.age + 2.308)
 
