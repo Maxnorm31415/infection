2033e5f8974099ac84ba8db7b16646718a8d7a52
Maxnorm31415 <kibalynik11@gmail.com>
2025-11-12 18:25:20 +0100

    work with aircraft

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from itertools import count
+from warnings import catch_warnings
+
 import pygame, random, math
 import numpy as np
 from person import Human, Infected
@@ -13,9 +16,21 @@ list_humans = []
 list_infected = []
 FRAMES_PER_SECOND = 60
 SPEED = 1
+virus_stage = 200
 
+change_virus_stage = False
 
-pygame.init()
+aircraft_left = pygame.image.load('airplane_left.png')
+aircraft_right = pygame.image.load('airplane_right.png')
+
+#For travel:
+travel = False
+passenger = Human(0,0,0,"none")
+aircraft = aircraft_left
+aircraft_pos = [0,0]
+vector = [0,0]
+continent_new = ""
+counter = 0
 
 screen_width, screen_height = 1600, 821
 
@@ -33,12 +48,12 @@ def make_continents():
     # Eurasia – 65.7%, Africa – 19.1%, North America – 8.9%, South America – 5.5%, Australia – 0.7%, Greenland – 0.1%.
     # bounds(x_min,y_min,x_max,y_max)
     continents = {
-        "south_america":  {"bounds": (277,434,535,748), "max_people": 20 + int(total_people * 0.089), "infection_rang":9},
-        "north_america":  {"bounds": (84,74,380,322), "max_people": 20 + int(total_people * 0.055), "infection_rang":6},
-        "eurasia":        {"bounds": (586,39,1447,268), "max_people": 20 + int(total_people * 0.657), "infection_rang":7},
-        "africa":         {"bounds": (636,306,988,710), "max_people": 20 + int(total_people * 0.191), "infection_rang":3},
-        "australia":      {"bounds": (1370,587,1589,737), "max_people": 20 + int(total_people * 0.007), "infection_rang":9},
-        "greenland":      {"bounds": (525,5,676,88), "max_people": 20 + int(total_people * 0.001), "infection_rang":6},
+        "south_america":  {"bounds": (277,434,535,748), "max_people": 20 + int(total_people * 0.089),"airport":(440,552), "infection_rang":9},
+        "north_america":  {"bounds": (84,74,380,322), "max_people": 20 + int(total_people * 0.055),"airport":(260,155), "infection_rang":6},
+        "eurasia":        {"bounds": (586,39,1447,268), "max_people": 20 + int(total_people * 0.657),"airport":(1034,235), "infection_rang":7},
+        "africa":         {"bounds": (636,306,988,710), "max_people": 20 + int(total_people * 0.191),"airport":(722,330), "infection_rang":3},
+        "australia":      {"bounds": (1370,587,1589,737), "max_people": 20 + int(total_people * 0.007),"airport":(1450,660), "infection_rang":9},
+        "greenland":      {"bounds": (525,5,676,88), "max_people": 20 + int(total_people * 0.001), "airport":(620,35), "infection_rang":6},
     }
     total_people = 0
     for continent in continents:
@@ -77,6 +92,41 @@ def infected(hum):
     list_infected.append(index)
     list_humans[index] = Infected(hum)
 
+def flying(surface):
+    global aircraft_pos, counter, travel, list_humans,list_infected
+    if counter != 100:
+        surface.blit(aircraft, (aircraft_pos[0], aircraft_pos[1]))
+        aircraft_pos[0] += vector[0]/100
+        aircraft_pos[1] += vector[1]/100
+        counter += 1
+    else:
+        min = 0
+        max = 0
+        for con in continents:
+            if con == continent_new:
+                max = min + continents[con]["max_people"]
+                break
+            else:
+                min += continents[con]["max_people"]
+        #print(max-1, continent_new)
+        passenger.pos_x = continents[continent_new]["airport"][0]
+        passenger.pos_y = continents[continent_new]["airport"][1]
+        passenger.continent = continent_new
+        passenger.id = max-1
+        list_infected.append(passenger.id)
+        list_humans.insert(max, passenger)
+        print(max, len(list_humans)-1)
+        for i in range(max,len(list_humans)-1):
+            if list_humans[i].infection:
+                list_infected.remove(list_humans[i].id)
+                list_infected.append(list_humans[i].id+1)
+            list_humans[i].id += 1
+        continents[continent_new]["max_people"] += 1
+        #for hu in list_humans:
+            #print(hu.id, hu.continent)
+        counter = 0
+        travel = False
+
 def found_neighbors(x,y,cont):
     list_neighbors = []
     min = 0
@@ -112,8 +162,44 @@ def draw_humans(surface):
     for hum in list_humans:
         pygame.draw.circle(surface, hum.color, (hum.pos_x, hum.pos_y), 4)
 
+#def infection_die(hum):
+
+def make_travel(hum):
+    global passenger, vector, travel,aircraft, continent_new, aircraft_pos
+    if hum.try_travel():
+        travel = True
+        passenger = hum
+        continent_new = hum.continent
+        start = [continents[hum.continent]["airport"][0],continents[hum.continent]["airport"][1]]
+        continents[hum.continent]["max_people"]-= 1
+        end = (0, 0)
+        finish = False
+        list_airports = []
+        for con in continents:
+            list_airports.append(con)
+        while not finish:
+            continent_new = random.choice(list_airports)
+            end = continents[continent_new]["airport"]
+            if end != start:
+                finish = True
+        aircraft_pos = start
+        vector = ((end[0] - start[0]), (end[1] - start[1]))
+        angle = math.degrees(math.atan2(vector[0], vector[1]))
+        if abs(angle) < 90:
+            aircraft = pygame.transform.rotate(aircraft_left, angle)
+        else:
+            aircraft = pygame.transform.rotate(aircraft_right, angle)
+        list_humans.remove(hum)
+        list_infected.remove(hum.id)
+        for i in range(hum.id, len(list_humans) - 1):
+            if list_humans[i].infection:
+                list_infected.remove(list_humans[i].id)
+                list_infected.append(list_humans[i].id - 1)
+            list_humans[i].id -= 1
+
 
 def move_humans(boards):
+    global change_virus_stage
     for hum in list_humans:
         finish = False
         while(not finish):
@@ -124,6 +210,17 @@ def move_humans(boards):
                 hum.pos_x = x
                 hum.pos_y = y
                 finish = True
+        if hum.infection and change_virus_stage:
+            hum.virus_live -= 1
+            if hum.virus_live == 0:
+                list_humans[hum.id] = Human(hum.id,hum.pos_x, hum.pos_y,hum.continent, False,hum.firstname,
+                                            hum.lastname, hum.age, hum.social_rang, hum.travel_rang,hum.immunity + 1)
+                list_infected.remove(hum.id)
+        if hum.infection and not travel and len(list_infected) < len(list_humans):
+            make_travel(hum)
+
+    if change_virus_stage:
+        change_virus_stage = False
 
 def speek():
     global list_infected
@@ -139,18 +236,24 @@ def speek():
                     infected(neighbor)
 
 def main():
-    global prin
+    global change_virus_stage, aircraft_left, aircraft_right
     screen = pygame.display.set_mode((screen_width,screen_height))
     pygame.display.set_caption("Human Infection")
     mask = pygame.mask.from_surface(pygame.image.load('mask.png').convert_alpha())
     world = pygame.image.load('world.png').convert()
+    aircraft_left = aircraft_right.convert_alpha()
+    aircraft_right = aircraft_left.convert_alpha()
     clock = pygame.time.Clock()
     mouse_pos = pygame.mouse.get_pos()
     make_continents()
     make_humans(mask)
     infected(random.choice(list_humans))
     count_speed = 0
+    count_virus = 0
     while True:
+        if len(list_infected) + len(list_humans) > total_people*2:
+            print("HAHAHAHAH",len(list_infected),len(list_humans))
+            pygame.quit()
         # event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -163,11 +266,28 @@ def main():
         #print(mouse_pos)
         screen.fill(BLACK)
         screen.blit(world, (0, 0))
+        if travel:
+            flying(screen)
         draw_humans(screen)
         count_speed += 1
+        if count_virus == virus_stage:
+            change_virus_stage = True
+            count_virus = 0
+        else:
+            count_virus += 1
         if (count_speed % SPEED == 0):
+           # print(len(list_infected))
             move_humans(mask)
-            speek()
+            try:
+                speek()
+            except Exception as e:
+                print("Humans id:")
+                for hu in list_humans:
+                    print(hu.id, hu.infection, hu.continent)
+                print("Infected:")
+                for inf in list_infected:
+                    print(inf)
+                pygame.quit()
             count_speed = 0
         pygame.display.update()
         clock.tick(FRAMES_PER_SECOND)
--- person.py
+++ person.py
@@ -2,6 +2,7 @@ import random
 
 DARK_GREEN = (14, 112, 7)
 RED = (176, 0, 0)
+BLUE = (0, 0, 255)
 
 class Human:
     id = 0
@@ -15,15 +16,16 @@ class Human:
     pos_y = 0
     continent = ""
     infection = False
+    die_chance = 0.00
     color = (0,0,0)
-    def __init__(self, id, x, y, cont,f_name = "None",l_name = "None",ag = 0,s_r = 0,t_r = 0, imun = 0):
+    def __init__(self, id, x, y, cont,inf = False,f_name = "None",l_name = "None",ag = 0,s_r = 0,t_r = 0, imun = 0):
         if f_name == "None":
             self.firstname = random.choice(["Alex", "Maya", "Leo", "Sophie", "Ethan", "Luna", "Oliver", "Isla", "Noah", "Aria"])
             self.lastname = random.choice(["Johnson", "Carter", "Bennett", "Miller", "Collins", "Smith", "Brown", "Davis", "Wilson", "Clark"])
             self.age = random.randint(15,80)
             self.social_rang = random.randint(1,10)
             self.travel_rang = random.randint(1,10)
-            self.color = DARK_GREEN
+            self.immunity= 0
         else:
             self.firstname = f_name
             self.lastname = l_name
@@ -32,14 +34,17 @@ class Human:
             self.social_rang = s_r
             self.travel_rang = t_r
             self.color = RED
+        if inf:
+            self.color = RED
+        elif self.immunity == 0:
+            self.color = DARK_GREEN
+        else:
+            self.color = BLUE
         self.id = id
         self.continent = cont
         self.pos_x = x
         self.pos_y = y
 
-    def try_speek(self, human):
-        chance = (0.1 * self.social_rang) * (0.1 * human.social_rang)
-        return random.uniform(0, 1) <= chance
 
     def try_infection(self):
         return random.uniform(0, 1) <= 0.5
@@ -48,7 +53,8 @@ class Infected(Human):
     virus_live = 0
     virus_level = 1
     def __init__(self, human):
-        super().__init__(human.id,human.pos_x, human.pos_y,human.continent,human.firstname, human.lastname, human.age, human.social_rang, human.travel_rang)
+        super().__init__(human.id,human.pos_x, human.pos_y,human.continent, True,human.firstname, human.lastname,
+                         human.age, human.social_rang, human.travel_rang)
         self.infection = True
         self.virus_live = int(0.346*human.age + 2.308)
 
@@ -56,3 +62,8 @@ class Infected(Human):
         chance = 0.015*self.travel_rang
         return random.uniform(0,1)<= chance
 
+    def try_speek(self, human):
+        if human.immunity < self.virus_level:
+            chance = (0.1 * self.social_rang) * (0.1 * human.social_rang)
+            return random.uniform(0, 1) <= chance
+        else: return False
